b36c0f99b2c69ed1753574a26868243bf572049c
Dhanya-krishnan2 <63437424+Dhanya-krishnan2@users.noreply.github.com>
2020-06-10 16:36:29 +0800

    Update script.js

--- script.js
+++ script.js
@@ -154,7 +154,7 @@ $(".saveBtn").on("click", function(event) {
     event.preventDefault();
 
     var saveIndex = $(this).siblings(".description").children(".future").attr("id");
-    myDay[saveIndex].reminder = $(this).siblings(".description").children(".future").val();
+    myPlannedDay[saveIndex].reminder = $(this).siblings(".description").children(".future").val();
     console.log(saveIndex);
     saveReminders();
     displayReminders();
